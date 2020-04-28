02adae1f21b301e246faf9d58e9c198c9984b17d
Carlo Vega <vega.carlo@uabc.edu.mx>
2020-04-27 20:13:03 -0700

    Agregado login/logout/borradores, mejorada seguridad..

--- meta1_5/settings.py
+++ meta1_5/settings.py
@@ -122,6 +122,10 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
